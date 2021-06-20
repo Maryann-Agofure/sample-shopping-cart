79aedcd2b725be092071d0a77caff0903fe5e02b
Maryann Agofure <maryannagofure@gmail.com>
2021-06-20 23:36:07 +0100

    deleted a counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = { 
         value: this.props.value,
-        items: ['dresses', "shoes", "gowns", "tops"]
+        items: ['watch', 'earrings']
         
      }
      handleIncrement =()=>{
@@ -26,11 +26,11 @@ class Counter extends Component {
 
         return (
             <div>
-            {this.state.items.length ===0 && 'Go to homage and start shopping!'}
+            {this.state.items.length ===0 && 'Go to home page and start shopping!'}
             {this.renderList()}
             <span className={this.updateClasses()} style= {{fontSize: 20}}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-md" onClick= {(product) => this.handleIncrement(product)}>Increment</button>
-            <button className = "btn btn-danger btn-sm m-2" onClick= {()=>this.props.onDelete(this.props.id)}>Delete</button>
+            <button className="btn btn-secondary btn-md" onClick= {() => this.handleIncrement()}>Increment</button>
+            <button className = "btn btn-danger btn-sm m-2" onClick= {() => this.props.onDelete(this.props.id)}>Delete</button>
             </div>
         )
         
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -6,13 +6,14 @@ class Counters extends Component {
         counters: [
             {"id":1, "value": 3},
             {"id":2, "value": 0},
-            {"id":3, "value": 6},
-            {"id":4, "value": 0}
+            {"id":3, "value": 6}
         ]
+     };
+     handleDelete = countersId => {
+        console.log('Delete a counter', countersId);
      }
-     handleDelete =(counterId) =>{
-         console.log('Delete a counter', counterId)
-     }
+         
+     
     render() { 
         return ( 
             <div>
