13ed7a587e3c3b7267ddb92ccef4e4a23adbf61b
Maryann Agofure <maryannagofure@gmail.com>
2021-06-22 21:22:48 +0100

    made some corrections

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = { 
-        value: this.props.value,
-        items: ['watch', 'earrings']
-        
-     }
-     handleIncrement =()=>{
-        this.setState({value: this.state.value + 1})
-        
-    }
+    
+    
     // constructor(){
       //   super()
         //this.handleIncrement = this.handleIncrement.bind(this)
          
      //}
-     renderList(){
-         if(this.state.items.length === 0)
-         return <p>oops! there are no items in your cart. Start shopping to add items</p>
-         return <ul>
-         {this.state.items.map(item=><li key={item}>{item}</li>)}
-         </ul>
-     }
+     
     render() { 
 
         return (
             <div>
-            {this.state.items.length ===0 && 'Go to home page and start shopping!'}
-            {this.renderList()}
+            
             <span className={this.updateClasses()} style= {{fontSize: 20}}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-md" onClick= {() => this.handleIncrement()}>Increment</button>
-            <button className = "btn btn-danger btn-sm m-2" onClick= {() => this.props.onDelete(this.props.id)}>Delete</button>
+            <button className="btn btn-secondary btn-md mr-4" onClick= {() => this.props.onIncrement(this.props.counter)}>Add to cart</button>
+            <button className="btn btn-secondary btn-md" onClick= {() => this.props.onDecrement(this.props.counter)}>Remove item from cart</button>
+            <button className = "btn btn-danger btn-sm m-2" onClick= {() => this.props.onDelete(this.props.counter.id)}>Delete</button>
             </div>
         )
         
@@ -38,13 +25,13 @@ class Counter extends Component {
     }
     
     formatCount(){
-        let {value } = this.state;
+        let {value } = this.props.counter;
         return(value ===0 ? "Zero": value )
     }
     updateClasses(){
         let classes = 'badge m-3 badge-'
         
-            return classes+= (this.state.value  === 0? "warning": "primary")
+            return classes+= (this.props.counter.value  === 0? "warning": "primary")
         
     }
     
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,26 +2,20 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            {"id":1, "value": 3},
-            {"id":2, "value": 0},
-            {"id":3, "value": 6}
-        ]
-     };
-     handleDelete = countersId => {
-        console.log('Delete a counter', countersId);
-     }
-         
+    
+    
      
     render() { 
+        const {onReset, onDelete, onIncrement, onDecrement} = this.props
         return ( 
             <div>
-            {this.state.counters.map(counter=>
+            <button className = "btn btn-primary btn-md m-2" onClick= {onReset}>Reset</button>
+            {this.props.counters.map(counter=>
                 <Counter key ={counter.id}
-                value = {counter.value}
-                selected = {true}
-                onDelete = {this.handleDelete}
+                counter = {counter} 
+                onDelete = {onDelete}
+                onIncrement = {onIncrement}
+                onDecrement = {onDecrement}
                 />
                 
                 )}
